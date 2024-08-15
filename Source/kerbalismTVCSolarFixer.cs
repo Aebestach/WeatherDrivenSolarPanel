cefc6403cd62dc00c3bb04a9075122fb3b4b0f72
Aebestach <aebestach@163.com>
2024-08-15 20:47:08 +0800

    upload

---
+++ Source/kerbalismTVCSolarFixer.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using HarmonyLib;
+using KERBALISM;
+
+namespace WeatherDrivenSolarPanel
+{
+    [HarmonyPatch(typeof(SolarPanelFixer))]
+    [HarmonyPatch("FixedUpdate")]
+    public static class kerbalismTVCSolarFixer
+    {
+        public static void Prefix(SolarPanelFixer __instance)
+        {
+            __instance.currentOutput++;
+            Debug.Log("Prefix: customVariable incremented to " + __instance.currentOutput);
+        }
+
+        public static void Postfix(SolarPanelFixer __instance)
+        {
+            if (__instance.currentOutput > 10)
+            {
+                Debug.Log("Postfix: customVariable is greater than 10");
+            }
+        }
+    }
+}
